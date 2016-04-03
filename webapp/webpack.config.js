81002cb8874fb55401f11d1dea72ebd696d2406c
Kjartan Bjørset <kjartanbjorset@gmail.com>
2016-04-03 22:30:20 +0200

    last

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -8,6 +8,12 @@ const PATHS = {
 };
 
 module.exports = {
+    resolveLoader: {                                                                                
+      root: path.join(__dirname+'../node_modules', 'node_modules')                                                  
+  },     
+   resolve: {
+
+   },
   // Entry accepts a path or an object of entries. We'll be using the
   // latter form given it's convenient with more complex configurations.
   entry: {
